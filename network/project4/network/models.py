371d3efb2736b99896e8cf72bdde94cab555aa96
DevHyperCoder <arduinoleo88@gmail.com>
2020-08-30 19:04:57 +0530

    Added Post Models

--- network/project4/network/models.py
+++ network/project4/network/models.py
@@ -1,6 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    creator = models.ForeignKey(User,default=1,on_delete=models.CASCADE)
+    content = models.CharField(max_length=500,default="")
+    created_at = models.DateTimeField(timezone.now)
+    likes = models.PositiveIntegerField(default=0)
