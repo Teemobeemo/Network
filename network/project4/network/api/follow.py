3c9fd36c68d3aaa982b304486f18e7c9e5329c99
DevHyperCoder <arduinoleo88@gmail.com>
2020-09-01 08:14:19 +0530

    Added Follow and Unfollow feature

---
+++ network/project4/network/api/follow.py
@@ -0,0 +1,43 @@
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+
+from network.models import User,Follow
+
+@login_required(login_url='login')
+def follow_api(request):
+    nextA= request.GET.get('next')
+    
+    current_user = request.user
+
+    to_follow = request.POST.get("to_follow")
+
+    to_follow_user = User.objects.get(username=to_follow)
+
+    following_user = Follow.objects.filter(user_id=current_user)
+    for user in following_user:
+        if (user.follow_id.username == to_follow):
+            return redirect(nextA)
+
+    follow = Follow(user_id=current_user,follow_id = to_follow_user)
+    follow.save()
+
+    return redirect(nextA)
+
+def unfollow_api(request):
+    nextA= request.GET.get('next')
+    
+    current_user = request.user
+
+    to_follow = request.POST.get("to_follow")
+
+    to_follow_user = User.objects.get(username=to_follow)
+
+    following_user = Follow.objects.filter(user_id=current_user)
+
+    for user in following_user:
+        if (user.follow_id.username == to_follow):
+            user.delete()
+            return redirect(nextA)
+    return redirect(nextA)
+
+
--- network/project4/network/urls.py
+++ network/project4/network/urls.py
@@ -4,10 +4,16 @@ from django.urls import path
 from . import views
 from . import home_views
 from . import profile_views
+from . import follow_views
+from .api import follow
 urlpatterns = [
     path("", home_views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    path("profile/",profile_views.profile,name='profile')
+    path("profile/", profile_views.profile, name='profile'),
+    path("following/", follow_views.follow_view, name='follow_views'),
+    path("api/follow/",follow.follow_api,name='follow_api'),
+    path("api/unfollow/",follow.unfollow_api,name='unfollow_api'),
+
 ]
