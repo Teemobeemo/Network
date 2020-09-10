386967a1a8d95ec483341cba53e1a1402bb410e7
DevHyperCoder <arduinoleo88@gmail.com>
2020-09-10 08:07:55 +0530

    Add get like feature

--- network/project4/network/api/like.py
+++ network/project4/network/api/like.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.test.client import JSON_CONTENT_TYPE_RE
 from network.models import Like,Post
@@ -50,3 +49,26 @@ def dislike_api(request):
     except Post.DoesNotExist:
         return JsonResponse({"error":"Post Does not Exist"})
 
+# REname to if_liek_api
+def get_like_api(request):
+    # Check if user is logged in
+    if not request.user.is_authenticated:
+        return JsonResponse({'error':"You should be logged in to like a post"})
+
+    # Get post id and user form the request body
+    post_id = request.GET.get('id')
+    user = request.user
+
+    # Get the post. If not found return a error
+    try:
+        post = Post.objects.get(id=post_id)
+
+        # Check if user liked the post and return appropriately
+        like_by_user = Like.objects.filter(user=user,post =post)
+        if like_by_user:
+            return JsonResponse({"status":"True"})
+        
+        return JsonResponse({"status":"False"})
+
+    except Post.DoesNotExist:
+        return JsonResponse({"error":"Post Does not Exist"})
