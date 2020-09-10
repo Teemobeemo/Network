1c04511149c06f16a7c5bdaf1eaf2295cbdfd037
DevHyperCoder <arduinoleo88@gmail.com>
2020-09-10 07:12:17 +0530

    Add dislike API

--- network/project4/network/api/like.py
+++ network/project4/network/api/like.py
@@ -27,11 +27,26 @@ def like_api(request):
     except Post.DoesNotExist:
         return JsonResponse({"error":"Post Does not Exist"})
 
-@login_required(login_url='login')
 def dislike_api(request):
-    return {"asd":"asdf"}
+    if not request.user.is_authenticated:
+        return JsonResponse({'error':"You should be logged in to like a post"})
+    post_id = request.GET.get('id')
+    user = request.user
+    try:
+        post = Post.objects.get(id=post_id)
 
-@login_required(login_url='login')
-def get_like_api(request):
-    return {"asd":"asdf"}
+        likes_user_post = Like.objects.filter(user =user,post = post)
+        if  not likes_user_post:
+            return JsonResponse({"error":"You didnt like it before "})
+        
+        likes_user_post.delete()
+
+        post.likes -=1
+        post.save()
+
+        response ={"likes":f"{post.likes}"}
+        return JsonResponse(response)
+
+    except Post.DoesNotExist:
+        return JsonResponse({"error":"Post Does not Exist"})
 
